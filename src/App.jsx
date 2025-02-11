659e4d1489d56dab89888db53552c93ca40a670e
Dmitry <duduff9317@gmail.com>
2025-02-11 14:26:27 +0300

    add Mock API

--- src/App.jsx
+++ src/App.jsx
@@ -6,68 +6,71 @@ import About from "./pages/About";
 import Order from "./pages/Order";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { customStorage } from "./localStorage/customStorage";
-import Modal from './modal/Modal'
+import Modal from "./modal/Modal";
 
 function App() {
-    const [orders, setOrders] = useState(
-        JSON.parse(localStorage.getItem("data")) || []
-    );
-    const [modalActive, setModalActive] = useState(true) //стейт для открытия модалки
-    const [modalDoubleText, setModalDoubleText] = useState('') //стейт для текста модалки
+  const [orders, setOrders] = useState(
+    JSON.parse(localStorage.getItem("data")) || []
+  );
+  const [modalActive, setModalActive] = useState(true); //стейт для открытия модалки
+  const [modalDoubleText, setModalDoubleText] = useState(""); //стейт для текста модалки
 
-    //подсчет итоговой суммы
-    const calculateTotalPrice = () => {
-        return orders.reduce((acc, el) => acc + el.price, 0);
-    };
+  //подсчет итоговой суммы
+  const calculateTotalPrice = () => {
+    return orders.reduce((acc, el) => acc + el.price, 0);
+  };
 
-    const totalPrice = calculateTotalPrice();
+  const totalPrice = calculateTotalPrice();
 
-    //добавление товара в корзину
-    const addToOrder = (el) => {
-        let inArray = false;
-        orders.forEach((item) => {
-            if (el.id === item.id) {
-                inArray = true;
-                setModalActive(false)
-                setModalDoubleText(`Вы уже добавили ${el.name} в корзину! Пожалуйста, выберите другой товар или оформите заказ.`)
-            }
-        });
-        if (!inArray) {
-            setOrders([...orders, el]);
-            customStorage([...orders, el]);
-        }
-    };
-
-    useEffect(() => {
-        console.log(orders);
+  //добавление товара в корзину
+  const addToOrder = (el) => {
+    let inArray = false;
+    orders.forEach((item) => {
+      if (el.id === item.id) {
+        inArray = true;
+        setModalActive(false);
+        setModalDoubleText(
+          `Вы уже добавили ${el.name} в корзину! Пожалуйста, выберите другой товар или оформите заказ.`
+        );
+      }
     });
+    if (!inArray) {
+      setOrders([...orders, el]);
+      customStorage([...orders, el]);
+    }
+  };
 
-    return (
-        <>
-            <BrowserRouter>
-                <Header orders={orders} />
-                <Modal text={modalDoubleText} modalActive={modalActive} setModalActive={setModalActive} />
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<Home addToOrder={addToOrder} />}
-                    />
-                    <Route path="/about" element={<About />} />
-                    <Route
-                        path="/order"
-                        element={
-                            <Order
-                                totalPrice={totalPrice}
-                                orders={orders}
-                                setOrders={setOrders}
-                            />
-                        }
-                    />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
-        </>
-    );
+  useEffect(() => {
+    console.log(orders);
+  });
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header orders={orders} />
+        <Modal
+          text={modalDoubleText}
+          modalActive={modalActive}
+          setModalActive={setModalActive}
+        />
+        <Routes>
+          <Route path="/" element={<Home addToOrder={addToOrder} />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/order"
+            element={
+              <Order
+                totalPrice={totalPrice}
+                orders={orders}
+                setOrders={setOrders}
+              />
+            }
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,60 +4,68 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { data } from "../../data/data";
-import './Home.css'
+// import { data } from "../../data/data";
+import { useState, useEffect } from "react";
+import "./Home.css";
 
 const Home = ({ addToOrder }) => {
-    return (
-        <>
-            <div className="home__picture">
-                <p>
-                    <a href="#bottom" className="btn">Перейти к меню</a>
-                </p>
-            </div>
-            <p>
-                <a name="bottom"></a>
-            </p>
-            <div
-                style={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    justifyContent: "space-between",
-                }}
-            >
+  const [data, setData] = useState([]);
 
-                {data.map((el) => (
-                    <Card key={el.id} sx={{ maxWidth: 345 }}>
-                        <CardMedia
-                            sx={{ height: 350 }}
-                            image={el.image}
-                            title={el.name}
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {el.name}
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                sx={{ color: "text.secondary" }}
-                            >
-                                {el.description}
-                            </Typography>
-                            <p>
-                                <span>Price:</span>
-                                {el.price}$
-                            </p>
-                        </CardContent>
-                        <CardActions>
-                            <Button onClick={() => addToOrder(el)} size="small">
-                                Добавить в корзину
-                            </Button>
-                        </CardActions>
-                    </Card>
-                ))}
-            </div>
-        </>
-    );
+  //подтягиваем пиццы с удаленного сервера
+  useEffect(() => {
+    try {
+      fetch("https://67ab24cd65ab088ea7e8d8d7.mockapi.io/pizza_place")
+        .then((response) => response.json())
+        .then((json) => setData(json));
+    } catch (error) {
+      console.error(`error to fetch $(error)`);
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="home__picture">
+        <p>
+          <a href="#bottom" className="btn">
+            Перейти к меню
+          </a>
+        </p>
+      </div>
+      <p>
+        <a name="bottom"></a>
+      </p>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {data.map((el) => (
+          <Card key={el.id} sx={{ maxWidth: 345 }}>
+            <CardMedia sx={{ height: 350 }} image={el.image} title={el.name} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {el.name}
+              </Typography>
+              <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                {el.description}
+              </Typography>
+              <p>
+                <span>Price:</span>
+                {el.price}$
+              </p>
+            </CardContent>
+            <CardActions>
+              <Button onClick={() => addToOrder(el)} size="small">
+                Добавить в корзину
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Home;
